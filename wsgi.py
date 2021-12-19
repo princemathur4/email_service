580ca3a246b3c6d9fc2111ab781bdd67172e1dc3
princemathur4 <princemathur.mathur4@gmail.com>
2021-12-19 07:50:23 +0530

    Minor fixes

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ from app import create_app, PROD, LOCAL
 env = PROD
 app = create_app(env)
 
-app.run(host="0.0.0.0", port="5000")
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port="5000")
